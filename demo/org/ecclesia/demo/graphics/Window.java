7fe1a0e70169908c7b739582422bb54dda5641e5
champfish <cj.duffee@gmail.com>
2016-04-13 01:27:47 +0000

    Demo

--- demo/org/ecclesia/demo/graphics/Window.java
+++ demo/org/ecclesia/demo/graphics/Window.java
@@ -3,12 +3,16 @@ package org.ecclesia.demo.graphics;
 import javax.swing.JFrame;
 
 public class Window extends JFrame {
+	/**
+	 * Instantiates the Window
+	 */
 	public Window() {
 		super("Line Predictor");
-		setSize(500, 500);
+		setSize(750, 750);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		add(new GraphicsFrame());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
 }
