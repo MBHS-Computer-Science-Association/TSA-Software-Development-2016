a9f727f34fc10de546e5b5f83d98b032db8d790d
Trevor Thai Kim Nguyen <trevortknguyen@gmail.com>
2016-06-26 18:34:08 -0500

    Migrating new demonstrations onto new platform.

--- demoTemplate/org/ecclesia/demoTemplate/DemoLauncher.java
+++ demoTemplate/org/ecclesia/demoTemplate/DemoLauncher.java
@@ -24,6 +24,8 @@ import javax.swing.text.JTextComponent;
 
 import org.ecclesia.demoGenetics.CancerGenomicAnalysis;
 import org.ecclesia.demoLines.LineExtrapolationDemo;
+import org.ecclesia.demoLogic.DigitalLogicTrainer;
+import org.ecclesia.demoMedical.MedicalAnalysisDemonstration;
 import org.ecclesia.demoSurvival.GameIntelligenceSimulation;
 
 /**
@@ -53,6 +55,8 @@ public final class DemoLauncher extends JFrame {
 		demoList.add(new CancerGenomicAnalysis());
 		demoList.add(new GameIntelligenceSimulation());
 		demoList.add(new LineExtrapolationDemo());
+		demoList.add(new DigitalLogicTrainer());
+		demoList.add(new MedicalAnalysisDemonstration());
 	}
 	
 	/**
