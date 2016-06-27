91841326fedc3087763f581b20bbd00a861975e9
nefaerious <samshin102@gmail.com>
2016-06-26 20:00:52 -0500

    Moving classes around

--- demoGenetics/org/ecclesia/demoGenetics/CancerGenomicAnalysis.java
+++ demoGenetics/org/ecclesia/demoGenetics/CancerGenomicAnalysis.java
@@ -1,6 +1,14 @@
 package org.ecclesia.demoGenetics;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 import org.ecclesia.demoTemplate.Demonstration;
 
@@ -8,10 +16,12 @@ import org.ecclesia.demoTemplate.Demonstration;
  * The functioning class that calls the GUI template and appends the 
  * proper content to the content panel and the proper content to the control panel
  * 
- * @author Sammy Shin and Trevor Nguyen
+ * @author Sammy Shin
  *
  */
 public class CancerGenomicAnalysis extends Demonstration {
+	private GeneticsLogic geneticsLogic;
+	
 	public CancerGenomicAnalysis() {
 		super("Artificial Neural Networks in Cancer Genetic Studies");
 		this.setIntroduction(Demonstration.getInstructionsFromFile(new File("demoGenetics/introduction.txt")));
@@ -19,7 +29,59 @@ public class CancerGenomicAnalysis extends Demonstration {
 
 	@Override
 	public void run() {		
+		geneticsLogic = new GeneticsLogic();
 		this.setContentPanel(new ContentPanel());
 		this.setControlPanel(new ControlPanel());
 	}
+	
+	class ControlPanel extends JPanel {
+
+		
+		public ControlPanel()
+		{	
+			createComponents();
+		}
+		
+		private void createComponents() {
+
+			GridLayout experimentLayout = new GridLayout(0,2);
+			setLayout(experimentLayout);
+
+			JButton train = new JButton("Train Current Neural Network");
+			JButton generate = new JButton("Generate New Neural Network");
+			
+			train.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					geneticsLogic.train();
+				}
+			});
+			
+			generate.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					geneticsLogic.setNetwork();
+				}
+			});
+			
+			add(train);
+			add(generate);
+
+		
+		}
+		
+	}
+	class ContentPanel extends JPanel {
+		
+		
+		public ContentPanel()
+		{	
+			createComponents();
+		}
+		
+		private void createComponents() {
+			
+		}
+	}
+
 }
